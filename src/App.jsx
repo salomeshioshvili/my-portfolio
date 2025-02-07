1ccfe8b40e4676d5a21d85726f41f876738d2141
salome shioshvili <79744349+salomeshioshvili@users.noreply.github.com>
2025-02-07 23:08:34 +0100

    Deployed changes to GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   return (
     <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello World!</h1>
+      <h1 className="text-4xl font-bold">HI!</h1>
     </div>
   );
 }
