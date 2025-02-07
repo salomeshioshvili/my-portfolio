c04bb94833d0b092e2d87e35b985122393b46b84
salome shioshvili <79744349+salomeshioshvili@users.noreply.github.com>
2025-02-07 21:26:24 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
